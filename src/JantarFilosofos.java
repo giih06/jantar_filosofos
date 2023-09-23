98e6d3afc1780a1f0a24e626acf0f1e548c2e44d
Giovanna Federico <giovannamfederico@gmail.com>
2023-09-23 14:44:56 -0300

    comentarios adicionais

--- src/JantarFilosofos.java
+++ src/JantarFilosofos.java
@@ -1,5 +1,9 @@
 import java.util.concurrent.Semaphore;  // Importa a classe Semaphore do pacote java.util.concurrent.
-
+// comentários explicando a classe semaphore:
+/*
+ * acquire() é utilizado para adquirir (ou bloquear) um semáforo
+ * release() é utilizado para liberar (ou desbloquear) um semáforo
+ */
 public class JantarFilosofos {
     public static void main(String[] args) {
         int numFilosofos = 5;  // Define o número de filósofos.
@@ -18,43 +22,47 @@ public class JantarFilosofos {
 }
 
 class Filosofo extends Thread {
+    // atributos
     private int id;  // Identificação do filósofo.
     private Semaphore garfoesquerdo;  // Semáforo para o garfo esquerdo.
     private Semaphore garfodireito;  // Semáforo para o garfo direito.
 
+    // construtor
     public Filosofo(int id, Semaphore garfoesquerdo, Semaphore garfodireito) {
         this.id = id;  // Inicializa a identificação do filósofo.
         this.garfoesquerdo = garfoesquerdo;  // Inicializa o semáforo do garfo esquerdo.
         this.garfodireito = garfodireito;  // Inicializa o semáforo do garfo direito.
     }
 
+    // metodo de rodar a aplicação
     public void run() {
         try {
             while (true) {  // Entra em um loop infinito para que o filósofo continue a agir.
-                pensar();  // Chama o método think para simular o pensamento do filósofo.
-                pegueGarfos();  // Chama o método pickUpForks para pegar os garfos e se preparar para comer.
-                comer();  // Chama o método eat para simular o ato de comer.
-                abaixarGarfos();  // Chama o método putDownForks para colocar os garfos de volta na mesa.
+                pensar();  // Chama o método pensar para simular o pensamento do filósofo.
+                pegueGarfos();  // Chama o método pegueGarfos para pegar os garfos e se preparar para comer.
+                comer();  // Chama o método comer para simular o ato de comer.
+                abaixarGarfos();  // Chama o método abaixarGarfos para colocar os garfos de volta na mesa.
             }
         } catch (InterruptedException e) {  // Tratamento de exceção para interrupção da thread.
             Thread.currentThread().interrupt();  // Marca a thread como interrompida.
         }
     }
 
+    // métodos encapsulados
     private void pensar() throws InterruptedException {
         System.out.println("Filosofo " + id + " está pensando.");  // Imprime mensagem indicando que o filósofo está pensando.
-        Thread.sleep((long) (Math.random() * 1000));  // Simula um período de pensamento (atraso aleatório).
+        Thread.sleep((long) (Math.random() * 1000));  // Simula um período de pensamento (atraso aleatório entre 0 e 999 milissegundos).O .sleep faz a thread atual pausar sua execução por um numero aleatorio do math.random. Esse (long) converte o Math.random() * 100 (que é um double) em um valor inteiro longo. O método Thread.sleep espera receber um argumento do tipo long, que representa a quantidade de milissegundos que a thread deve dormir
     }
 
     private void pegueGarfos() throws InterruptedException {
-        garfoesquerdo.acquire();  // Tenta adquirir o semáforo do garfo esquerdo (bloqueia se não disponível).
+        garfoesquerdo.acquire();  // Tenta adquirir a permissao do semáforo do garfo esquerdo (bloqueia se não disponível).
         System.out.println("Filosofo " + id + " pega o garfo esquerdo.");  // Imprime mensagem indicando que o filósofo pegou o garfo esquerdo.
         garfodireito.acquire();  // Tenta adquirir o semáforo do garfo direito (bloqueia se não disponível).
         System.out.println("Filosofo " + id + " pega o garfo direito.");  // Imprime mensagem indicando que o filósofo pegou o garfo direito.
     }
 
     private void comer() throws InterruptedException {
-        System.out.println("Filosofo " + id + " is eating.");  // Imprime mensagem indicando que o filósofo está comendo.
+        System.out.println("Filosofo " + id + " está comendo.");  // Imprime mensagem indicando que o filósofo está comendo.
         Thread.sleep((long) (Math.random() * 1000));  // Simula um período de alimentação (atraso aleatório).
     }
 
